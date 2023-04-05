ca82546292044e2337208d9f49b3f90e53299b38
Fernando <gilfernando648@gmail.com>
2023-04-05 08:35:47 -0700

    LoaderYMessage2

--- src/Componentes/CrudApi.js
+++ src/Componentes/CrudApi.js
@@ -1,42 +1,55 @@
 import { useEffect, useState } from "react"
 import CrudForm from "./CrudForm"
 import CrudTable from "./CrudTable"
-import {helpHttp} from "../helper/helpHttp";
+import { helpHttp } from "../helper/helpHttp";
+import Loader from "./Loader";
+import Message from "./Message";
 
-const CrudApi =() => {
+const CrudApi = () => {
     //Variables
-    let api=helpHttp();
-    let url='http://localhost:5000/users'
+    let api = helpHttp();
+    let url = 'http://localhost:5000/users'
 
     useEffect(() => {
+        setLoading(true)
         api.get(url).then(response => {
             //console.log(response)
-            if(!response.err){
+            if (!response.err) {
+                setDb(response)
+            }
+            else {
                 setDb(null)
             }
+            setLoading(false)
         })
     }, [])
 
-    const [db, setDb] = useState([])
-    const [dataToEdit,setDataToEdit]=useState(null)
-    const createData=(data) =>{
-        data.id=db.length;
-        setDb([...db,data])
+    const [db, setDb] = useState(null)
+    const [dataToEdit, setDataToEdit] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    const createData = (data) => {
+        data.id = db.length;
+        setDb([...db, data])
     };
-    const updateData=(data) =>{
-        let newData=db.map(item => item.id==data.id?data:item)
+    const updateData = (data) => {
+        let newData = db.map(item => item.id == data.id ? data : item)
         setDb(newData)
     }
-    const deleteData=(id) =>{
-        let eliminar=db.filter(item=>item.id!==id)
+    const deleteData = (id) => {
+        let eliminar = db.filter(item => item.id !== id)
         setDb(eliminar)
     };
 
     return (
         <div>
             <h1>CrudApi</h1>
-            <CrudForm create={createData} update={updateData} dataToEdit={dataToEdit} setDataToEdit={setDataToEdit}/>
-            <CrudTable data={db} setDataToEdit={setDataToEdit} deleteData={deleteData}/>
+            <CrudForm create={createData} update={updateData} dataToEdit={dataToEdit} setDataToEdit={setDataToEdit} />
+
+            {loading && <Loader />}
+            {error && <Message />}
+            {db && <CrudTable data={db} setDataToEdit={setDataToEdit} deleteData={deleteData} />}
         </div>
     )
 }
---
+++ src/Componentes/Message.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Message =() => {
+    return(
+        <div>
+            <h1>Message</h1>
+        </div>
+    )
+}
+export default Message
